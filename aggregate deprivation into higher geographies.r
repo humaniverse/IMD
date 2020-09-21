59b76be5c4c908abc7bee46ac9f76080c73e9c0f
Matt Thomas <matthewgthomas@gmail.com>
2020-09-21 16:35:44 +0100

    Added LA-level IMD for Scotland

--- aggregate deprivation into higher geographies.r
+++ aggregate deprivation into higher geographies.r
@@ -9,7 +9,7 @@ source("functions.r")
 
 imd_uk = read_csv("data/UK IMD domains.csv")
 
-# ---- LSOA population estimates ----
+# ---- LSOA population estimates for England and Wales ----
 GET("https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2flowersuperoutputareamidyearpopulationestimates%2fmid2019sape22dt2/sape22dt2mid2019lsoasyoaestimatesunformatted.zip",
     write_disk(tf <- tempfile(fileext = ".zip")))
 
@@ -22,6 +22,13 @@ pop = read_excel("data/population/SAPE22DT2-mid-2019-lsoa-syoa-estimates-unforma
 pop = pop %>% 
   select(Code = `LSOA Code`, `No. people` = `All Ages`)
 
+# ---- Data Zones population estimates for Scotland ----
+# source: https://www.opendata.nhs.scot/dataset/population-estimates/resource/c505f490-c201-44bd-abd1-1bd7a64285ee
+pop_sco = read_csv("https://www.opendata.nhs.scot/dataset/7f010430-6ce1-4813-b25c-f7f335bdc4dc/resource/c505f490-c201-44bd-abd1-1bd7a64285ee/download/dz2011-pop-est_01092020.csv")
+
+pop_sco = pop_sco %>%
+  filter(Year == 2019 & Sex == "All") %>% 
+  select(Code = DataZone, `No. people` = AllAges)
 
 # ---- Load LSOA to LAD lookup ----
 lsoa_lad = read_csv("https://opendata.arcgis.com/datasets/fe6c55f0924b4734adf1cf7104a0173e_0.csv")
@@ -51,3 +58,21 @@ wimd_la = wimd %>%
 
 # Save
 write_csv(wimd_la, "data/Welsh IMD - Local Authorities.csv")
+
+
+# ---- Scotland ----
+simd = imd_uk %>% 
+  filter(str_sub(LSOA, 1, 1) == "S") %>% 
+  select(Code = LSOA, IMD_rank, IMD_decile) %>% 
+  
+  mutate(IMD_score = 0) %>%  # don't have IMD scores for Wales so just add a dummy column
+  
+  left_join(lsoa_lad, by = c("Code" = "LSOA11CD")) %>% 
+  left_join(pop_sco, by = "Code")
+
+# Aggregate into LAs
+simd_la = simd %>% 
+  aggregate_scores(domain = "IMD", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "LAD19CD")
+
+# Save
+write_csv(simd_la, "data/Scottish IMD - Local Authorities.csv")
