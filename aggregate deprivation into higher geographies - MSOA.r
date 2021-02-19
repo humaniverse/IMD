7302b562bc791e61319e83da46c3d6b4369766bd
Matt Thomas <matthewgthomas@gmail.com>
2021-02-19 12:35:10 +0000

    Included MSOA aggregation for all English domains of deprivation

--- aggregate deprivation into higher geographies - MSOA.r
+++ aggregate deprivation into higher geographies - MSOA.r
@@ -51,13 +51,42 @@ lsoa_lad = lsoa_lad %>%
 # ---- England ----
 # File 7: all ranks, deciles and scores for the indices of deprivation, and population denominators
 # from https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019
-eimd = read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/845345/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv")
+eimd_raw = read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/845345/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv")
 
-eimd = eimd %>% 
+eimd = eimd_raw %>% 
   select(Code = `LSOA code (2011)`, 
+         
          IMD_score = `Index of Multiple Deprivation (IMD) Score`, 
          IMD_rank = `Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)`, 
-         IMD_decile = `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`) %>% 
+         IMD_decile = `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`,
+         
+         Income_score = `Income Score (rate)`,
+         Income_rank = `Income Rank (where 1 is most deprived)`,
+         Income_decile = `Income Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Employment_score = `Employment Score (rate)`,
+         Employment_rank = `Employment Rank (where 1 is most deprived)`,
+         Employment_decile = `Employment Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Education_score = `Education, Skills and Training Score`,
+         Education_rank = `Education, Skills and Training Rank (where 1 is most deprived)`,
+         Education_decile = `Education, Skills and Training Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Health_score = `Health Deprivation and Disability Score`,
+         Health_rank = `Health Deprivation and Disability Rank (where 1 is most deprived)`,
+         Health_decile = `Health Deprivation and Disability Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Crime_score = `Crime Score`,
+         Crime_rank = `Crime Rank (where 1 is most deprived)`,
+         Crime_decile = `Crime Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Housing_and_Access_score = `Barriers to Housing and Services Score`,
+         Housing_and_Access_rank = `Barriers to Housing and Services Rank (where 1 is most deprived)`,
+         Housing_and_Access_decile = `Barriers to Housing and Services Decile (where 1 is most deprived 10% of LSOAs)`, 
+         
+         Environment_score = `Living Environment Score`,
+         Environment_rank = `Living Environment Rank (where 1 is most deprived)`,
+         Environment_decile = `Living Environment Decile (where 1 is most deprived 10% of LSOAs)`) %>% 
   
   left_join(lsoa_lad, by = c("Code" = "LSOA11CD")) %>% 
   left_join(pop, by = "Code")
@@ -74,6 +103,31 @@ eimd = eimd %>%
 # Aggregate intoMSOAs
 eimd_msoa = eimd %>% 
   aggregate_scores(domain = "IMD", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+
+eimd_msoa_income   = eimd %>% aggregate_scores(domain = "Income", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_employ   = eimd %>% aggregate_scores(domain = "Employment", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_edu      = eimd %>% aggregate_scores(domain = "Education", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_health   = eimd %>% aggregate_scores(domain = "Health", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_crime    = eimd %>% aggregate_scores(domain = "Crime", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_barriers = eimd %>% aggregate_scores(domain = "Housing_and_Access", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+eimd_msoa_env      = eimd %>% aggregate_scores(domain = "Environment", score_suffix = "_score", rank_suffix = "_rank", decile_suffix = "_decile", aggregate_by = "MSOA11CD")
+
+eimd_msoa_income   = eimd_msoa_income   %>% rename(Income_Proportion = Proportion, Income_Extent = Extent, Income_Score = Score)
+eimd_msoa_employ   = eimd_msoa_employ   %>% rename(Employment_Proportion = Proportion, Employment_Extent = Extent, Employment_Score = Score)
+eimd_msoa_edu      = eimd_msoa_edu      %>% rename(Education_Proportion = Proportion, Education_Extent = Extent, Education_Score = Score)
+eimd_msoa_health   = eimd_msoa_health   %>% rename(Health_Proportion = Proportion, Health_Extent = Extent, Health_Score = Score)
+eimd_msoa_crime    = eimd_msoa_crime    %>% rename(Crime_Proportion = Proportion, Crime_Extent = Extent, Crime_Score = Score)
+eimd_msoa_barriers = eimd_msoa_barriers %>% rename(Housing_and_Access_Proportion = Proportion, Housing_and_Access_Extent = Extent, Housing_and_Access_Score = Score)
+eimd_msoa_env      = eimd_msoa_env      %>% rename(Environment_Proportion = Proportion, Environment_Extent = Extent, Environment_Score = Score)
+
+eimd_msoa = eimd_msoa %>% 
+  left_join(eimd_msoa_income,   by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_employ,   by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_edu,      by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_health,   by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_crime,    by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_barriers, by = "MSOA11CD") %>% 
+  left_join(eimd_msoa_env,      by = "MSOA11CD")
 
 # Save
 write_csv(eimd_msoa, "data/English IMD - MSOA.csv")  
