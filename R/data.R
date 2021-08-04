6e7cf82a2f4c7fd1ec4d28be59d0ade7e0cf62dd
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2021-08-04 14:29:55 +0100

    Added IMD for SOAs in NI

--- R/data.R
+++ R/data.R
@@ -85,3 +85,32 @@
 #' }
 #' @source \url{https://www.gov.scot/}
 "imd_scotland_dz"
+
+#' Index of Multiple Deprivation for Super Output Areas in Northern Ireland
+#'
+#' A data set containing IMD ranks and deciles for SOAs in Northern Ireland.
+#'
+#' @format A data frame of class "tbl" with 890 rows and 17 variables:
+#' \describe{
+#'   \item{soa_code}{Super Output Area code}
+#'   \item{IMD_decile}{IMD decile}
+#'   \item{Income_decile}{Income domain decile}
+#'   \item{Employment_decile}{Employment domain decile}
+#'   \item{Education_decile}{Education domain decile}
+#'   \item{Health_decile}{Health and Disability domain decile}
+#'   \item{Crime_decile}{Crime domain decile}
+#'   \item{Access_decile}{Access to services domain decile}
+#'   \item{Environment_decile}{Environment domain decile}
+#'   \item{IMD_rank}{IMD rank}
+#'   \item{Income_rank}{Income domain rank}
+#'   \item{Employment_rank}{Employment domain rank}
+#'   \item{Education_rank}{Education domain rank}
+#'   \item{Health_rank}{Health domain rank}
+#'   \item{Crime_rank}{Crime domain rank}
+#'   \item{Housing_rank}{Housing domain rank}
+#'   \item{Access_rank}{Access to services domain rank}
+#'   \item{Environment_rank}{Environment domain rank}
+#'   ...
+#' }
+#' @source \url{https://www.nisra.gov.uk/}
+"imd_northern_ireland_soa"
